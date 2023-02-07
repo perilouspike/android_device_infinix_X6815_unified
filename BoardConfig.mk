141e4d0918378f9707715b69bf9c2bb0610bc66b
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-02-07 11:31:22 +0530

    Update BoardConfig.mk
    Changed the path os system.prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,7 +131,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USES_MKE2FS := true # Use mke2fs to create ext4 images
-TARGET_SYSTEM_PROP := $(DEVICE_PATH)/props/system.prop
+TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 
 # Verified Boot
