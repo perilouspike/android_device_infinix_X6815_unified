67e089021c8de56dd3d51d063e88deebfbac0b56
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-02-21 17:07:39 +0000

    Fix build error.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -34,11 +34,11 @@ local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
 
 }
 
-   if [ -z "$1" ] && [ -z "$FOX_BUILD_DEVICE" ]; then
+   if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 	fox_get_target_device
 fi
 
-if [ "$1" = "$FDEVICE" ] || [  "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	# Initial Exports
 	export ALLOW_MISSING_DEPENDENCIES=true
 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
@@ -173,14 +173,12 @@ if [ "$1" = "$FDEVICE" ] || [  "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
                 unset "FOX_USE_SPECIFIC_MAGISK_ZIP"
             fi
         fi
-    fi
-fi
 
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-	export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-	export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-	export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-	export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+	  export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+	  export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+	  export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+	  export | grep "TW_" >> $FOX_BUILD_LOG_FILE
 	fi
 fi
